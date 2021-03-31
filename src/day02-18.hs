9b16e870f29f4df269273e232a1c5220badab656
Semyon <zielinski@fastmail.com>
2021-03-31 14:10:52 +0200

    hmph

--- src/day02-18.hs
+++ src/day02-18.hs
@@ -1,3 +1,18 @@
+{-# LANGUAGE TupleSections #-}
+
+import qualified Data.Map as M
+import Data.Containers.ListUtils
+import Data.List
+-- pretty sick frequency map 
+
+freqs :: Ord a => [a] -> M.Map a Int
+freqs = M.fromListWith (+) . map (,1)
+
+day02a :: Ord a => [[a]] -> [Int]
+day02a xs = filter (> 1) $ concatMap (nubOrd . M.elems . freqs) xs
+
+
 main :: IO ()
 main = do
-  xs <- readFile "../resources/dat18/day02.txt"
+  xs <- lines <$> readFile "../resources/dat18/day02.txt"
+  print $ foldr (*) 1 $ map length $ group . sort $ day02a xs
